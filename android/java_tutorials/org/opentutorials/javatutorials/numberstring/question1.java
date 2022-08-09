d60efab8800cad9780f247497d9a21a452f1efa4
김우현 <mrpc2003@gmail.com>
2022-08-09 21:18:58 +0900

    java

---
+++ android/java_tutorials/org/opentutorials/javatutorials/numberstring/question1.java
@@ -0,0 +1,25 @@
+package org.opentutorials.javatutorials.numberstring;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class question1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> numList = new ArrayList<Integer>();
+		numList.add(sc.nextInt());
+		numList.add(sc.nextInt());
+		numList.add(sc.nextInt());
+		numList.add(sc.nextInt());
+		numList.add(sc.nextInt());
+		numList.add(sc.nextInt());
+		int sum = 0;
+		for(int i:numList)
+		{
+			sum += i;
+		}
+		System.out.println("합계: " + sum + ", 최대: " + Collections.max(numList) + ", 최소: " + Collections.min(numList) + ", 평균: " + (double)sum / numList.size());
+	}
+
+}
