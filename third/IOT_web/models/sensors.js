738020979f052a9d13ebc8cee579d20bce39d57e
김우현 <mrpc2003@gmail.com>
2022-08-11 15:11:51 +0900

    3주차 과제 완성

--- third/IOT_web/models/sensors.js
+++ third/IOT_web/models/sensors.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
-const SensorsSchema = mongoose.Schema({
-  tmp: {
+const mongoose = require("mongoose"); // mongoose 모듈 불러오기
+const SensorsSchema = mongoose.Schema({ // 설정한 스키마 정의
+  tmp: { 
     type: String,
-    required: true,
-  },
+    required: true, 
+  }, // 온도
   hum: {
     type: String,
     required: true,
-  },
+  }, // 습도
   pm1: {
     type: String,
     require: true,
-  },
+  }, // pm1
   pm2: {
     type: String,
     require: true,
-  },
+  }, // pm2.5
   pm10: {
     type: String,
     require: true,
-  },
+  }, // pm10
   created_at: {
     type: Date,
     default: Date.now,
-  },
+  }, // 생성 시간
 });
-module.exports = mongoose.model("Sensors", SensorsSchema);
+module.exports = mongoose.model("Sensors", SensorsSchema); // 모델 내보내기
--- third/IOT_web/routes/devices.js
+++ third/IOT_web/routes/devices.js
@@ -1,21 +1,22 @@
 var express = require("express"); // express 모듈 불러오기
 var router = express.Router(); // express 라우터 모듈 불러오기
 const mqtt = require("mqtt"); // mqtt 모듈 불러오기
-const Sensors = require("../models/sensors");
-// MQTT Server 접속
-const client = mqtt.connect("mqtt://192.168.1.48"); // 라즈베리파이 url
+const Sensors = require("../models/sensors"); // 센서 모델 불러오기
+// MQTT Server 접속 설정
+const client = mqtt.connect("mqtt://192.168.1.48"); // 라즈베리파이 url 입력 (서버 주소)
 //웹에서 rest-full 요청받는 부분(POST) web - > ras pi -> arduino -> led control
 router.post("/led", function (req, res, next) {
+  // led 제어 함수
   res.set("Content-Type", "text/json"); // 응답 타입 설정
   if (req.body.flag == "on") {
     // MQTT->led : 1
-    client.publish("led", "1"); // LED ON
-    res.send(JSON.stringify({ led: "on" }));//웹에서 전송받은 데이터를 다시 전송
+    client.publish("led", "1"); // LED ON 전송
+    res.send(JSON.stringify({ led: "on" })); //웹에서 전송받은 데이터를 다시 전송
   } else {
     // MQTT->led : 2
 
-    client.publish("led", "2");// LED OFF
-    res.send(JSON.stringify({ led: "off" }));//웹에서 전송받은 데이터를 다시 전송
+    client.publish("led", "2"); // LED OFF 전송
+    res.send(JSON.stringify({ led: "off" })); //웹에서 전송받은 데이터를 다시 전송
   }
 });
 module.exports = router; // 모듈 내보내기
